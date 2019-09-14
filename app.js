f39c4d340a809f0dcb8b9e965622d0b42af0098c
Hamza Zoumhani <zoumhanihamza@gmail.com>
2019-09-14 11:41:33 -0500

    added some paths

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.get('/helloworld', function(req, res){
     res.send("Hello World!");
 });
 
+app.get('/helloworld', function(req, res){
+    res.send("This is not the right hello world!");
+});
+
+app.get('/hello/world', function(req, res){
+    res.send("This is another Hello World!");
+});
 
 app.listen(port, function(){
     console.log("Express running on port: " + port);
