6fb624d9d5b157d1d032d3c2655849adadf16b69
Hamza Zoumhani <zoumhanihamza@gmail.com>
2019-09-14 12:53:32 -0500

    more

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.use('/', express.static('files'));
+
 app.get('/helloworld', function(req, res){
     res.json({ message: 'hello'});
 });
@@ -12,6 +14,22 @@ app.get('/helloworld', function(req, res){
 
 app.get('/hello/world', function(req, res){
     res.send("This is another Hello World!");
+});
+
+app.get('/message/1', function(req, res){
+    res.json({message: "This is message one"});
+});
+
+app.get('/message/1.thing', function(req, res){
+    res.json({message: "This is message one dot thing"});
+});
+
+app.get('/message/*', function(req, res){
+    res.json({message: "This is every other message"});
+});
+
+app.get('/message/for/id:', function(req, res){
+    res.json({message: "This is message one"});
 });
 
 app.listen(port, function(){
